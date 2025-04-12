d607b225acb7950aacbfbf430a8a1a7b881fb375
herry-dev <barrybomhead@gmail.com>
2025-04-12 11:56:25 +0100

    Delete gmonapi directory

--- gmonapi/players/player.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request, jsonify
-
-charbp = Blueprint('character', __name__, url_prefix='/char')
-
-@charbp.route('/<char>', methods=['GET'])
-def get_player(char):
-    return jsonify({"name":char})
